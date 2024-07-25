e70f3d8a071db3ac9d334edd6bac259efade9d4a
melnard <dejesusmelnard@gmail.com>
2024-07-26 00:35:10 +0800

    added animation

---
+++ src/scripts/animationUponRender.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var sections = document.querySelectorAll(".animated-section");
+
+  sections.forEach((section) => {
+    section.classList.add("show");
+  });
+});
