702f770ab01325ef19eedb4e1d840e9c0fb4a354
Melnard <dejesusmelnard@gmail.com>
2024-08-09 07:34:23 +0800

    added sitemap to the package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig, squooshImageService } from "astro/config";
-
 import preact from "@astrojs/preact";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   image: {
     service: squooshImageService(),
   },
-  integrations: [preact()],
+  integrations: [preact(), sitemap()],
+  site: "https://jayandmikka-wedding.netlify.app/",
 });
