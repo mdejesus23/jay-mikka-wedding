69b22474a9e23284f8cb98722fd41eab1cfa13c6
melnard <dejesusmelnard@gmail.com>
2024-10-20 11:55:31 +0800

    use sharp for image optimazation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, squooshImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 
 import sitemap from "@astrojs/sitemap";
@@ -6,8 +6,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jayandmikka-wedding.netlify.app/",
-  image: {
-    service: squooshImageService(),
-  },
   integrations: [preact(), sitemap()],
 });
